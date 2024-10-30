a5e0f5ddb29146210953a4f0a7fe7f859a653314
jorgedss <jorgesouza621@gmail.com>
2024-10-30 15:15:07 -0300

    Inserindo validação

--- src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,8 +1,14 @@
 package br.com.jorgedss.gestao_vagas.modules.candidate;
 
-import java.util.UUID;
 
+import java.util.UUID;
+import jakarta.validation.constraints.Email;
+
+
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
 
 @Data
 
@@ -11,8 +17,14 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um email válido")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
     private String description;
     private String curriculum;
