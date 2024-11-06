16b92935e0a6efa36b5f15536e537c7e9420b6e2
jorgedss <jorgesouza621@gmail.com>
2024-11-06 13:38:20 -0300

    Validando usuário já existente

--- src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,8 @@
 package br.com.jorgedss.gestao_vagas.modules.candidate.controllers;
 
+import br.com.jorgedss.gestao_vagas.exceptions.UserFoundException;
 import jakarta.validation.Valid;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +17,18 @@ public class CandidateController {
 
     @Autowired
     private CandidateRepository candidateRepository;
+
     @PostMapping("/")
     public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+       this.candidateRepository
+               .findByUsernameOrEmail(candidateEntity.getUsername(),candidateEntity.getEmail())
+               .ifPresent((user)->{
+                   throw new UserFoundException();
+               });
+
+
        return this.candidateRepository.save(candidateEntity);
+
     }
     
 }
--- src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/controllers/CandidateRepository.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/controllers/CandidateRepository.java
@@ -3,7 +3,9 @@ package br.com.jorgedss.gestao_vagas.modules.candidate.controllers;
 import br.com.jorgedss.gestao_vagas.modules.candidate.CandidateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+ Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
