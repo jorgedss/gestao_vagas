2409f4c3be185a940e8f1550a69598702696bc25
jorgedss <jorgesouza621@gmail.com>
2024-11-06 10:55:15 -0300

    Criando tabela de candidato

--- src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,20 +1,30 @@
 package br.com.jorgedss.gestao_vagas.modules.candidate;
 
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 
+
 @Data
-
-
+@Entity(name = "candidate")
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+
     
     private UUID id;
     private String name;
@@ -30,6 +40,9 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     
 }
