095777ec8c5991a18606708db04481c73c1fad99
jorgedss <jorgesouza621@gmail.com>
2024-11-12 14:15:53 -0300

    Job Controller

--- src/main/java/br/com/jorgedss/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -3,8 +3,10 @@ package br.com.jorgedss.gestao_vagas.modules.company.controllers;
 import br.com.jorgedss.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import br.com.jorgedss.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.jorgedss.gestao_vagas.modules.company.useCases.CreateCompanyUseCases;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +20,12 @@ public class CompanyController {
     private CreateCompanyUseCases createCompanyUseCases;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity){
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity){
         try {
             var result = this.createCompanyUseCases.execute(companyEntity);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
+            e.printStackTrace();
             return  ResponseEntity.badRequest().body(e.getMessage());
         }
 
--- src/main/java/br/com/jorgedss/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,6 +1,7 @@
 package br.com.jorgedss.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -14,6 +15,8 @@ public class JobEntity {
     private UUID id;
     private String description;
     private String benefits;
+
+    @NotBlank(message = "Esse campo é obrigatório")
     private String level;
 
 
@@ -21,7 +24,7 @@ public class JobEntity {
     @JoinColumn(name = "company_id",insertable = false,updatable = false)
     private CompanyEntity companyEntity;
 
-    @Column(name = "company_id")
+    @Column(name = "company_id",nullable = false)
     private UUID companyId;
 
     @CreationTimestamp
