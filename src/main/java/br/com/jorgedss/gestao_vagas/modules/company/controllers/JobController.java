56fc4840401705a76f0009d8e4b26f503da9f3f4
jorgedss <jorgesouza621@gmail.com>
2024-11-19 21:47:04 -0300

    Refatorando JobController

--- src/main/java/br/com/jorgedss/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,14 +1,18 @@
 package br.com.jorgedss.gestao_vagas.modules.company.controllers;
 
+import br.com.jorgedss.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.jorgedss.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.jorgedss.gestao_vagas.modules.company.useCases.CreateJobUseCase;
 import jakarta.persistence.SqlResultSetMapping;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -18,7 +22,17 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public JobEntity create(@Valid @RequestBody JobEntity jobEntity){
+    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
+       var companyId =  request.getAttribute("company_id");
+
+
+        var jobEntity = JobEntity.builder()
+                .companyId(UUID.fromString(companyId.toString()))
+                .benefits(createJobDTO.getBenefits())
+                .description(createJobDTO.getDescription())
+                .level(createJobDTO.getLebel())
+                .build();
+
         return this.createJobUseCase.execute(jobEntity);
 
     }
--- src/main/java/br/com/jorgedss/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/company/entities/JobEntity.java
@@ -2,13 +2,19 @@ package br.com.jorgedss.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 @Data
 @Entity(name = "job")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
