7e43c85ac54cd2b2bdab902e04b04a59bf75de0a
jorgedss <jorgesouza621@gmail.com>
2024-11-20 09:49:35 -0300

    Hash da senha do candidato

--- src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/jorgedss/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -5,6 +5,7 @@ import br.com.jorgedss.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.jorgedss.gestao_vagas.modules.candidate.controllers.CandidateRepository;
 import br.com.jorgedss.gestao_vagas.modules.company.entities.CompanyEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,8 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public CandidateEntity execute(CandidateEntity candidateEntity){
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(),candidateEntity.getEmail())
@@ -20,6 +23,9 @@ public class CreateCandidateUseCase {
                 });
 
 
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+
+        candidateEntity.setPassword(password);
         return this.candidateRepository.save(candidateEntity);
     }
 }
